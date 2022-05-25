2f141cd8faf5cfb98f014a76f31ba3fb1fe9e51a
mirzoo <mirzo.okhunov@gmail.com>
2022-05-25 13:21:31 +0300

    add: BlockAdvesting

---
+++ BlockAdvesting/script/scritp.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__nav').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+
+});
